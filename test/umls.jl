aa2aaaf08eca35db002b1d17b9ea970292915464
Maria Isabel Restrepo <isabelrestre@gmail.com>
2017-11-08 15:38:51 -0500

    disable umls test if environment variables are not available
    - ENV are not available from forks

--- test/umls.jl
+++ test/umls.jl
@@ -4,6 +4,7 @@
     user= ""
     psswd = ""
     apikey = ""
+    
     try
         user = ENV["UMLS_USER"]
         psswd = ENV["UMLS_PSSWD"]
@@ -12,22 +13,24 @@
         warn("UMLS tests could not run: Set up credentials as enviroment variables: UMLS_USER, UMLS_PSSWD and UMLS_APIKEY")
     end
 
-    term = "obesity"
-    query = Dict("string"=>term, "searchType"=>"exact" )
+    if (user != "") && (psswd != "") && (apikey != "")
+        term = "obesity"
+        query = Dict("string"=>term, "searchType"=>"exact" )
 
 
-    tgt = get_tgt(force_new=true, username=user, password=psswd)
-    #do it again to test reading from file
-    tgt = get_tgt(username=user, password=psswd)   
-    #test apikey
-    tgt = get_tgt(force_new=true, apikey=apikey)
+        tgt = get_tgt(force_new=true, username=user, password=psswd)
+        #do it again to test reading from file
+        tgt = get_tgt(username=user, password=psswd)   
+        #test apikey
+        tgt = get_tgt(force_new=true, apikey=apikey)
 
-    @testset "Testing Search/Content/CUI" begin
-        all_results= search_umls(tgt, query)
-        @test length(all_results[1]["result"]["results"]) == 2
-        cui = best_match_cui(all_results)
-        @test cui == "C0028754"
-        sm = get_semantic_types(tgt, cui)
-        @test sm[1] == "Disease or Syndrome"
+        @testset "Testing Search/Content/CUI" begin
+            all_results= search_umls(tgt, query)
+            @test length(all_results[1]["result"]["results"]) == 2
+            cui = best_match_cui(all_results)
+            @test cui == "C0028754"
+            sm = get_semantic_types(tgt, cui)
+            @test sm[1] == "Disease or Syndrome"
+        end
     end
 end
