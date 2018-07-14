5b3b22276837b55d8f2a5d6f012d11188d9107ce
yuifu <harukao.cb@gmail.com>
2018-07-15 00:56:04 +0900

    fix gggenome test

--- test/gggenome.jl
+++ test/gggenome.jl
@@ -4,10 +4,7 @@
         query = "TTCATTGACAACATT"
         res = gggsearch(query, db="felCat5", format = "html")
         @test res.status == 200
-        @test startswith(Dict(res.headers)["Content-Type"], "text/xml")
-        body = parse_xml(String(res.body))
-        @test isa(body, XMLDict.XMLDictElement)
-        @test first(body)[1] != "ERROR"
+        @test startswith(Dict(res.headers)["Content-Type"], "text/html")
 
         res = gggsearch(query, db="felCat5", format = "txt", output = "toString")
         @test typeof(res) == String
