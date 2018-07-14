9ee9e2e66ab2534d2a3380505fb8d064e4746dd1
yuifu <harukao.cb@gmail.com>
2018-07-14 21:08:21 +0900

    add cases for toString and extractTopHit

--- test/gggenome.jl
+++ test/gggenome.jl
@@ -1,16 +1,23 @@
 
 @testset "GGGenome" begin
     @testset "gggsearch" begin
-        query = "GTGCGGTAACGCGACCGATCCCGGAGAAGCCGGCGGGA"
-        res = gggsearch(query, db="mm10", format = "txt")
+        query = "TTCATTGACAACATT"
+        res = gggsearch(query, db="felCat5", format = "txt")
         @test res.status == 200
         @test startswith(Dict(res.headers)["Content-Type"], "text/xml")
         body = parse_xml(String(res.body))
         @test isa(body, XMLDict.XMLDictElement)
         @test first(body)[1] != "ERROR"
+
+        res = gggsearch(query, db="felCat5", format = "txt", output = "toString")
+        @test typeof(res) == String
+
+        res = gggsearch(query, db="felCat5", format = "txt", output = "extractTopHit")
+        @test typeof(res) == String
     end
 
     @testset "gggdbs" begin
        @test  length(gggdbs()) > 0
     end
+
 end
